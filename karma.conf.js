8642c86caa2f0fd87c3acd9b4b3d58592a17b298
Max Lynch <max@drifty.com>
2017-03-20 12:34:21 -0500

    Working

--- karma.conf.js
+++ karma.conf.js
@@ -4,19 +4,19 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
       require('karma-mocha-reporter'),
-      require('angular-cli/plugins/karma')
+      require('@angular/cli/plugins/karma')
     ],
     files: [
       { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
-      './src/test.ts': ['angular-cli']
+      './src/test.ts': ['@angular/cli']
     },
     mime: {
       'text/x-typescript': ['ts','tsx']
@@ -26,10 +26,6 @@ module.exports = function (config) {
         html: 'coverage',
         lcovonly: './coverage/coverage.lcov'
       }
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['mocha', 'karma-remap-istanbul']
