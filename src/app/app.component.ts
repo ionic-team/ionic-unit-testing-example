7c5acab7fae4b4377407c53269849fb7e9ed3ccf
Daniel Sogl <daniel@sogls.de>
2017-05-30 21:50:11 +0200

    refactor(app.component.ts) unified templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,7 @@ export class MyApp {
 
   }
 
-  initializeApp() {
+  ionViewDidLoad() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
