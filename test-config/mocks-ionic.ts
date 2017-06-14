23e34c99b887b3abf0dc32d44b78c06913bc250b
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-06-15 00:21:41 +0930

    added mocks that support tabs with lazy loading

--- test-config/mocks-ionic.ts
+++ test-config/mocks-ionic.ts
@@ -77,3 +77,39 @@ export class SplashScreenMock extends SplashScreen {
     return;
   }
 }
+
+export class NavMock {
+ 
+  public pop(): any {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
+ 
+  public push(): any {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
+ 
+  public getActive(): any {
+    return {
+      'instance': {
+        'model': 'something',
+      },
+    };
+  }
+ 
+  public setRoot(): any {
+    return true;
+  }
+
+  public registerChildNav(nav: any): void {
+    return ;
+  }
+
+}
+
+export class DeepLinkerMock {
+
+}
