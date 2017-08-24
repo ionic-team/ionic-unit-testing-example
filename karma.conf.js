e6bf6abaa16686b2eb7d1ae9bc87a51671ed6cd4
David Atencia <david.atencia@gmail.com>
2017-08-24 18:55:20 +0200

    remove karma config file at the root folder
    Keep all the configuration files inside the `test-config` folder

--- karma.conf.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./test-config/karma.conf.js');
--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -2,7 +2,7 @@ var webpackConfig = require('./webpack.test.js');
 
 module.exports = function(config) {
   var _config = {
-    basePath: '',
+    basePath: '../',
 
     frameworks: ['jasmine'],
 
