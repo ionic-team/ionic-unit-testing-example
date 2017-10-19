4d922dc33e3349761fee94c6b33aabd49b364e12
Daniel <mytechde@outlook.com>
2017-10-19 18:37:27 +0200

    bump deps and enable lazyloading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Nav, Platform } from 'ionic-angular';
 
 
 @Component({
@@ -13,15 +10,15 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = 'Page1';
 
   pages: Array<{ title: string, component: any }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Page One', component: 'Page1' },
+      { title: 'Page Two', component: 'Page2' }
     ];
 
   }
---
+++ src/pages/page2/page2.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+
+import { Page2 } from './page2';
+
+@NgModule({
+  declarations: [
+    Page2,
+  ],
+  imports: [
+    IonicPageModule.forChild(Page2),
+  ],
+})
+export class Page2Module { }
