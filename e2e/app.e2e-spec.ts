ee22fa225bd198ce59e6104e1edb2615dbee0e7a
Devin Shoemaker <devinshoe@gmail.com>
2018-02-24 19:39:52 -0600

    Fix Protractor test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -12,9 +12,9 @@ describe('App', () => {
       page.navigateTo('/');
     });
 
-    it('should have a title saying Page One', () => {
+    it('should have a title saying Ionic App', () => {
       page.getTitle().then(title => {
-        expect(title).toEqual('Page One');
+        expect(title).toEqual('Ionic App');
       });
     });
   })
